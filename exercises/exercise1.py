8fc3913f9233bd0df4bdb3050d789bb90e1cdb83
Kamila Krasniewska <kamila.krasniewska@o2.pl>
2024-03-13 11:04:14 +0100

    dodanie trojkata

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -4,9 +4,17 @@ pole_kw = bok_a * bok_a
 
 print("Pole kwadratu wynosi: " + str(pole_kw))
 
+
 bok_x = 10
 bok_y = 15.5
 
 pole_prostokat = bok_x * bok_y
 
 print("Pole prostokata wynosi: " + str(pole_prostokat))
+
+
+bok_b = 15
+wysokosc = 10
+pole_trojkat = 1/2 * bok_b * wysokosc
+
+print("Pole trojkata wynosi: " + str(pole_trojkat))
